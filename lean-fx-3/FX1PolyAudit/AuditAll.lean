f3afd5bb239bf21ab26241de65fdf430de3195a6
Grigory Evko <grigory@evko.io>
2026-07-11 04:03:08 +0200

    AuditAll: wire the multi-obstruction Anick r3 gate into the umbrella

--- lean-fx-3/FX1PolyAudit/AuditAll.lean
+++ lean-fx-3/FX1PolyAudit/AuditAll.lean
@@ -1067,6 +1067,7 @@ import FX1PolyAudit.Polygraph.Homology.FreeGroupReducedWord
 import FX1PolyAudit.Polygraph.Homology.CrossedModuleCyclicThree
 import FX1PolyAudit.Polygraph.Homology.CrossedModuleIdentityInvariant
 import FX1PolyAudit.Polygraph.Homology.CyclicThreeAnickChains
+import FX1PolyAudit.Polygraph.Homology.MultiObstructionAnickChains
 import FX1PolyAudit.Polygraph.TwoCategory.Table.ContextClosure
 import FX1PolyAudit.Polygraph.TwoCategory.Table.FrobeniusSeed
 import FX1PolyAudit.Polygraph.TwoCategory.Table.WalkerMigration
